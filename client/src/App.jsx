e31e8af3712223a71453a167b6ec55a2f4888cd8
Shreyas2545 <shreyashadawale25@gmail.com>
2025-09-28 19:31:35 +0530

    created react app and node app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import React from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className="text-center text-2xl text-blue-500">Hello</div>
+    </>
+  )
+}
+
+export default App
