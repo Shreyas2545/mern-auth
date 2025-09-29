c59e202de5f98462a63633ca0ff49590a6332f9a
Shreyas2545 <shreyashadawale25@gmail.com>
2025-09-29 19:58:33 +0530

    designed login page

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,13 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const login = () => {
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/login", { email, password })
+      .then((result) => {
+        console.log("Response from backend:", result.data);
+        navigate("/home");
+      })
+      .catch((error) => console.log("Error:", error));
+  };
+
   return (
-    <div>
-      <>
-      <p className='text-4xl'>Successfully Logged in !!!</p>
-      </>
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
+          Login to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                required
+                autoComplete="email"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-900">
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                type="password"
+                name="password"
+                required
+                autoComplete="current-password"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Don't have an account?{" "}
+          <Link to="/signup" className="font-semibold text-indigo-600 hover:text-indigo-500">
+            Sign Up
+          </Link>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default login
+export default Login;
--- server/index.js
+++ server/index.js
@@ -13,6 +13,21 @@ app.post('/register',(req,res)=>{
     .then(user => res.json(user))
     .catch(err => res.json(err))
 })
+app.post('/login',(req,res)=>{
+    const {email,password} = req.body;
+    UserModel.findOne({email:email})
+    .then(user =>{
+        if (user) {
+            if(user.password === password){
+                res.json("Success !!!")
+            }else{
+                res.json("Password is Incorrect!!!")
+            }
+        }else{
+            res.json("No Record Existed !!!")
+        }
+    })
+})
 
 app.listen(3001,()=>{
     console.log("server is running!!!");
